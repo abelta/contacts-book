2685f1768cbe5db7567b5324a35aa3cf7f29df30
Abel Tamayo <atamayo@paradigmadigital.com>
2020-08-18 14:47:51 +0200

    Included new properties for a contact.

--- src/mocks/contact.js
+++ src/mocks/contact.js
@@ -3,6 +3,14 @@ import faker from 'faker';
 const contact = () => ({
   id: faker.random.number(),
   name: faker.fake("{{name.firstName}} {{name.lastName}}"),
+  country: {
+    name: faker.address.country(),
+    code: faker.address.countryCode(),
+  },
+  city: faker.address.city(),
+  phone: faker.phone.phoneNumber(),
+  email: faker.internet.email(),
+  avatar: faker.image.avatar(),
 });
 
 const contacts = ({ page }) => ({
